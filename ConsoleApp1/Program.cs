9dc884bbbc62d48c6197f0898e3c01445719633d
jakub.strakos <jakub.strakos@U4-10>
2023-10-18 12:13:16 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*byte pocetDevri = 11;
+            pocetDevri = 11;
+            int pocetObyvatel = 28412561;
+            float delkaDesky = 6.4F;
+            string nazevMesta = "Olomouc";
+            viceradkovi
+                kometar
+            const float koeficient = 4.16F;
+            bool prsi = false;
+            char znak = '?';
+            konverze mezi dat. typy, retezec na cislo
+            string pocetBodu = "654";
+            int pocedBodu1 = int.Parse(pocetBodu);
+            cislo na retezec
+            int hodnota = -500000;
+            string y = hodnota.ToString();
+            float protokol = 12.87F;
+            int protoko2 = Convert.ToInt32(protokol);
+            Console.WriteLine(protoko2);
+            Console.ReadKey(); */
+            /*
+            //1.
+            long hodnota = -12000000000;
+            //2.
+            string t = Convert.ToString(hodnota);
+            Console.WriteLine(t);
+            //3.
+            string maleCislo = "3,71";
+            //4.
+            float r = float.Parse(maleCislo);
+            Console.WriteLine(r);
+            //5.
+            char pismeno = 'g';
+            //6.
+            int pismeno1 = pismeno;
+            Console.WriteLine(pismeno1);
+            //7.
+            bool rozhodnuti = true;
+            //8.
+            float sirka = 4.51F;
+            //9.
+            int sirka_cela = Convert.ToInt32(sirka);
+            Console.WriteLine(sirka_cela);
+            //10.
+            char bod = (char)97;
+            Console.WriteLine(bod);
+            //nebo
+            char bod1 = Convert.ToChar(97);
+            Console.WriteLine(bod1);
+            //11.
+            const float mena = 9.51f;
+            Console.WriteLine(mena);
+
+
+            Console.ReadKey();
+            
+            char deska = '*';
+            Console.WriteLine(deska);
+            char stul = (char)106;
+            Console.WriteLine(stul);
+            bool dvere;
+
+            dvere = false;
+            Console.WriteLine(dvere);
+
+            long velka_castka = 300000000;
+            Console.WriteLine(velka_castka);
+            int dluh = -200000;
+
+            string dluh2 = Convert.ToString(dluh);
+            Console.WriteLine(dluh2);
+            float okno = 1.164f;
+
+            int okno2 = Convert.ToInt32(okno);
+            Console.WriteLine(okno2);
+            string vypocet = "41,6";
+         
+
+            float vypocet2 = float.Parse(vypocet);
+            Console.WriteLine(vypocet2);
+
+            const float prah = 6.11f;
+            Console.WriteLine(prah);
+            
+            int penize = 600;
+            Console.WriteLine(" " + penize + " " + "Kč");
+            Console.WriteLine(" {0} Kč", penize);
+            Console.WriteLine();
+            int m1 = 6;
+            int m2 = 60;
+            int vysledek = m1 + m2;
+            //1.
+            Console.WriteLine(m1 + "+" + m2 + "=" + vysledek);
+            //2.
+            Console.WriteLine("{0}+{1}={2}", m1, m2, vysledek);
+            //"\n" - odradkovani
+            //"\t" - odsazeni
+            Console.WriteLine("\nPocasi\n\n");
+            Console.WriteLine("\t\tKomin");
+            Console.WriteLine("Pondeli\n\tUtery\n\t\tStreda\n\t\t\tCtvrtek\n\t\t\t\tPatek\n");
+            */
+            // Výpis znaků \ ", změna barev výxtupu na konzoli
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
