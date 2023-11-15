dc65e051f228c08e640209a6315dfcd89c25773f
Jakub STRAKOŠ <willer66.kuba.strakos@gmail.com>
2023-11-15 12:10:57 +0100

    new upd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -106,25 +106,124 @@ namespace ConsoleApp1
             Console.WriteLine("\nPocasi\n\n");
             Console.WriteLine("\t\tKomin");
             Console.WriteLine("Pondeli\n\tUtery\n\t\tStreda\n\t\t\tCtvrtek\n\t\t\t\tPatek\n");
-            */
+            
             // Výpis znaků \ ", změna barev výxtupu na konzoli
             
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Panelák\n");
+            Console.ResetColor();
+            Console.WriteLine("Strom\n");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Letadlo\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("topol\tbříza\nakát\tjasmín");
+            Console.ResetColor();
+            int vaha = 20;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Váha: {0} kg", vaha);
+            Console.ResetColor();
+            */
+            /*
+            // 1.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("zima\n\tjaro\n\t\tléto\n\t\t\tpodzim");
+            // 2.
+            double vyska = 180;
+            Console.WriteLine("Výška: {0} cm", vyska);
+            // 3.
+            string nazev = "Ovoce";
+            string spolecnost = "s.r.o.";
+            string zamereni = "dovoz jižního ovoce";
+            Byte doba = 15;
+
+            Console.WriteLine("Jm. firmy:\t{0}, {1}\nČinnost:\t{2}\nNa trhu:\t{3} let", nazev, spolecnost, zamereni, doba);
+
+            // 4.
+            Console.WriteLine("Dnes se již neobejdeme bez \"moderních technologií\".");
+
+            // 5.
+            Console.Write("Barevný televizor značky ");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("L");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("G");
+            Console.ResetColor();
+            */
+            /*
+            Console.WriteLine("Program secte a odecte 2 desetina cisla");
             
+            //definovani
+            Console.Write("Zadej 1. desetine cislo: ");
+            float InpOne = float.Parse(Console.ReadLine());
+            Console.Write("Zadej 2. desetine cislo: ");
+            float InpTwo = float.Parse(Console.ReadLine());
+            //vypocet
+            float OutP = InpOne + InpTwo;
+            float OutM = InpTwo - InpOne;
+            Console.WriteLine("Soucet = {0}\nRoydil = {1}", OutP, OutM);
+            
+            //2.
+            Console.WriteLine("Vypocet hodnoty linearni funkce y = 6 * x -5");
+            Console.Write("Zadejte cislo x: ");
+            double x = double.Parse(Console.ReadLine());
+            double y = 6 * x - 5;
+            Console.WriteLine("y = {0}", y);
+            
+            //3.
+            int a = 30;
+            int b = 408;
+            Console.WriteLine("a: {0}, b: {1}", a, b);
 
+            (a, b) = (b, a);
+            Console.WriteLine("a: {0}, b: {1}", a, b);
 
+            Console.Write("Zadej velikost polomeru kruhu (cm): ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("Zadej pocet desetinych mist vysledku: ");
+            int des_mist = int.Parse(Console.ReadLine());
+            double obvod = 2 * Math.PI * r;
+            double obsah = Math.PI * r * r;
+            obvod = Math.Round(obvod, des_mist);
+            obsah = Math.Round(obsah, des_mist);
+            Console.WriteLine("Obvod = {0} cm\nObsah = {1} cm²", obvod, obsah);
+            
+            Console.Write("zadejte 1. celé číslo: ");
+            int delenec = int.Parse(Console.ReadLine());
+            Console.Write("zadejte 2. celé číslo: ");
+            int delitel = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (delitel == 0)
+            {
+                Console.WriteLine("Nulou dělit nelze.");
+            }
+            else
+            {
+                Console.Write("Zadej pocet desetinych mist vysledku: ");
+                int des_mist = int.Parse(Console.ReadLine());
+                double podil = (double)delenec / (double)delitel;
+                podil = Math.Round(podil, des_mist);
+                Console.WriteLine("Výsledek dělení je: {0}", podil);
+            }
+            
+            Console.Write("zadejte délku strany a v cm: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("zadejte délku strany b v cm: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("zadejte délku strany c v cm: ");
+            double c = double.Parse(Console.ReadLine());
 
-
-
-
-
-
-
-
-
-
+            double objem = a * b * c;
+            Console.WriteLine("Objem kvádru je: {0} cm", objem);
+            double povrch = 2 * (a * b + a * c + b * c);
+            Console.WriteLine("Povrch kvádru je: {0} cm", povrch);
+            */
 
 
             Console.ReadKey();
+
 
 
 
